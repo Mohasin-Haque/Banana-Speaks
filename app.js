7df9b866929c97173ecd3e2964df9251883bc3f5
Mohasin-Haque <mohasinhaque23121@gmail.com>
2021-07-27 20:16:11 +0530

    done homework

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var btnTranslate = document.querySelector ("#btn-translate");
 var txtInput = document.querySelector("#text-input");
 
 console.log(txtInput);
- btnTranslate.addEventListener("click", function clickEventHandler(){
+
+ function clickHandler(){
 console.log("clicked")
 console.log("input", txtInput.value)
- });
+ };
+
+ btnTranslate.addEventListener("click", clickHandler)
+
+ 1. document.querySelector("textarea")
+ 2. .btn-primary
+ 3. #input-btn
+ 4. "#input{name = 'translator'}"
