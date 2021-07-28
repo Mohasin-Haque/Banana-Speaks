8d109edbb52fd17c157af18afb75be94a0cc1578
Mohasin-Haque <mohasinhaque23121@gmail.com>
2021-07-28 10:34:51 +0530

    Completed app

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-var btnTranslate = document.querySelector ("#btn-translate");
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
-// outputDiv.innerText = "Hello Mohasin"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-// console.log(outputDiv);
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input;
+}
 
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("somethig went wrong.. please try again.")
+}
 
- function clickHandler(){
-outputDiv.innerText = "ajsjsjsjsjs  " + txtInput.value;
- };
+function clickHandler() {
+  // outputDiv.innerText = "ajsjsjsjsjs  " + txtInput.value;
 
- btnTranslate.addEventListener("click", clickHandler)
+  var inputText = txtInput.value; // taking input
 
- 
+  //calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText; //output
+     
+        
+    })
+    .catch(errorHandler)
+};
 
+btnTranslate.addEventListener("click", clickHandler);
